07b7dcb985a457494000281ac8cb7aaa6834358d
william200626 <william200626@hotmail.ca>
2026-08-27 17:09:27 -0400

    Create cours2.py

---
+++ cours2.py
@@ -0,0 +1,17 @@
+# auteur: william levasseur lafrenière
+# date: 2026-08-27
+# description: démonstration cours #2
+
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+nom = "Emma"
+age = 18
+# print() peut accepter plusieurs arguments entre les ( )
+print("Bonjour", nom, ", tu as", age, "ans")
+
+# Chaîne formatée, ou « f-string »
+message = f"Bonjour {nom}, tu as {age} ans."
+print(message)
