9556236ebe364b0067721e46891146ed6383dbfb
william200626 <william200626@hotmail.ca>
2026-09-22 13:32:46 -0400

    teste1
    teste1

--- clinique vétérinaire.py
+++ clinique vétérinaire.py
@@ -2,24 +2,27 @@
 #date: 2026-09-17
 #description: TP Clinique vétérinaire
 
-REQUIN_TEMP_MIN = 22
-REQUIN_TEMP_MAX = 26
-REQUIN_MASSE_MIN = 60
-REQUIN_MASSE_MAX = 150
+REQUIN_TEMP_MIN = 22.0
+REQUIN_TEMP_MAX = 26.0
+REQUIN_MASSE_MIN = 60.0
+REQUIN_MASSE_MAX = 150.0
 REQUIN_ADULTE = 60
 REQUIN_SENIOR = 240
-TIGRE_TEMP_MIN = 37,5
-TIGRE_TEMP_MAX = 39
-TIGRE_MASSE_MIN = 100
-TIGRE_MASSE_MAX = 260
+TIGRE_TEMP_MIN = 37.5
+TIGRE_TEMP_MAX = 39.0
+TIGRE_MASSE_MIN = 100.0
+TIGRE_MASSE_MAX = 260.0
 TIGRE_ADULTE = 36
 TIGRE_SENIOR = 144
-GNOU_TEMP_MIN = 37,5
-GNOU_TEMP_MAX = 39
-GNOU_MASSE_MIN = 120
-GNOU_MASSE_MAX = 270
+GNOU_TEMP_MIN = 37.5
+GNOU_TEMP_MAX = 39.0
+GNOU_MASSE_MIN = 120.0
+GNOU_MASSE_MAX = 270.0
 GNOU_ADULTE = 36
 GNOU_SENIOR = 180
+
+
+
 
 import math
 
@@ -31,18 +34,39 @@ Gnou = 3
 print("----------------INFORMATION SUR LE SPÉCIMEN----------------")
 
 Nom =str(input("Nom de l'animal : "))
-Espece =int(input("Espèce de l'animal : "))
+Espece =int(input("Espèce de l'animal (1 = requin, 2 = tigre, 3 = gnou): "))
 Âge_total_en_mois =int(input("Âge de l'animal (en mois) : "))
 Masse_en_livres =float(input("Masse de l'animal (en livres) : "))
-masse_en_kg=  0.45359237
-
-
 Température_corporelle_en_Fahrenheit =float(input("Température corporelle de l'animal (en °F) : "))
 
+masse_en_kg =  Masse_en_livres * 0.45359237
+nombre_année = Âge_total_en_mois // 12
+restant_mois = Âge_total_en_mois % 12
+Température_corporelle_en_celcius = (Température_corporelle_en_Fahrenheit - 32) *(5/9)
 
-print(Nom)
+
+print("======================================================================")
+print(f"                    CLINIQUE VÉTÉRINAIRE EXOTIQUE")
+print(f"                         DES ÎLES ST-MAURICE")
+print("======================================================================")
 if (Espece == Requin):
-    print("Requin")
+    print(f"Patient                   : {Nom} (requin)")
+    if (Âge_total_en_mois < REQUIN_SENIOR):
+        print(f"Âge                       : {nombre_année} ans et {restant_mois} mois (adulte)")
+    else:
+        print(f"Âge                       : {nombre_année} ans et {restant_mois} mois (sénior)")
+
+print("Saisie                    : masse en lbs, température en °F")
+print("----------------------------------------------------------------------")
+print("mesure                                  valeur    Norme")
+print(f"Température (°C)                         {Température_corporelle_en_celcius:.2f}    {REQUIN_TEMP_MIN}-{REQUIN_TEMP_MAX}")
+print(f"Masse (kg)                              {masse_en_kg:.2f}    {REQUIN_MASSE_MIN}-{REQUIN_MASSE_MAX}")
+print("----------------------------------------------------------------------")
+print("conversions")
+print(f"Masse          :         {Masse_en_livres:.2f} lbs =    {masse_en_kg:.2f} kg")
+print(f"Température    :         {Température_corporelle_en_Fahrenheit:.2f} °F   =    {Température_corporelle_en_celcius:.2f} °C")
+
+
 
 if (Espece == Tigre):
     print("Tigre")
@@ -51,11 +75,11 @@ if (Espece == Gnou):
     print("Gnou")
 
 
-print(f"{Âge_total_en_mois // 12} ans   {Âge_total_en_mois % 12} mois")
-
-print(f"{Masse_en_livres} lbs =  {Masse_en_livres / 0.45359237}")
 
 
+print(f"{Masse_en_livres} lbs =  {masse_en_kg} kg")
+
+print(f"{Température_corporelle_en_Fahrenheit} °F =   {Température_corporelle_en_celcius}°C")
 
 
 
---
+++ while.py
@@ -0,0 +1,82 @@
+
+
+
+# valeur = input("veux tu etre mon ami?").upper().strip()
+
+# if(valeur == "OUI"):
+#     print("merci!")
+# else:
+#     print("sorry!")
+
+# -------------------------------------------------------
+
+# NOMBRE_CACHE = 5
+
+# while True:
+#     nombre = int(input("inscrire un nombre"))      #sa c mal
+#     if(nombre == NOMBRE_CACHE):
+#         print("trouvé")
+#         break
+
+# print("fin")
+
+
+
+
+
+
+
+# -------------------------------------------------------
+ 
+# compteur = 1
+
+
+# while compteur < 5:
+#     compteur = compteur + 1
+#     print(f"tourne! tour#{compteur}")
+    
+# print("fin")
+
+# -------------------------------------------------------
+
+# compteur = 1
+
+# NOMBRE_CACHE = 5
+
+# valeur_de_devinette = int(input("inscrire un chiffre:"))
+
+# while valeur_de_devinette != NOMBRE_CACHE:
+#     print("Essaye encore!")
+#     valeur_de_devinette = int(input("inscrire un autre chiffre"))
+    
+    
+# print("fin")
+
+# -------------------------------------------------------
+
+# compteur = 10
+
+# while compteur > 0:
+#     print(compteur)
+#     compteur = compteur -1
+
+# print("décolage")
+
+# # -------------------------------------------------------
+
+# i = 0
+# j = 0
+
+# taille = int(input("quelle est la taille du carré?"))
+
+# while i < taille:
+#     j = 0
+#     while j < taille:
+#         print("*", end="")
+#         j += 1
+#     i = i + 1
+#     print("")
+
+
+
+# print("fin")
